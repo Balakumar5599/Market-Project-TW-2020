9e8d4c4773af2442a3a8e9a7f5f3861f704b073f
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-08-15 19:16:02 +0530

    Api is also updated with respect to the model file

--- marketplace_api.py
+++ marketplace_api.py
@@ -1,5 +1,5 @@
 from flask import request,jsonify,url_for,redirect,Flask
-from marketplace_methods import*
+from marketplace_model import*
 
 app=Flask(__name__)
 app.config['DEBUG']=True
@@ -8,33 +8,33 @@ app.config['DEBUG']=True
 def user_login():
     username=request.form['user_name']
     password=request.form['password']
-    return login(username,password)
+    return User(username,password).login_status()
 
 @app.route('/categories',methods=['GET'])
 def get_category():
-    return jsonify(get_categories())
+    return jsonify(Category.get_categories())
 
 @app.route('/categories/<category_id>/items',methods=['GET'])
 def get_category_item(category_id):
-    return jsonify(get_items(category_id))
+    return jsonify(Item.get_items(category_id))
 
 @app.route('/cart/<user_id>',methods=['GET'])
 def get_cart_item(user_id):
-    return jsonify(get_cart_details(user_id))
+    return jsonify(CustomerCart.get_cart_details(user_id))
 
 @app.route('/cart/<user_id>',methods=['POST'])
 def add_to_cart(user_id):
     item_id=request.form['item_id']
     quantity=request.form['quantity']
-    return insert_to_cart(user_id,item_id,quantity)
+    return CustomerCart.insert_to_cart(user_id,item_id,quantity)
 
 @app.route('/cart/<cart_id>',methods=['PATCH'])
 def update_cart_item_quantity(cart_id):
     quantity=request.form['quantity']
-    return update_quantity(cart_id,quantity)
+    return CustomerCart.update_quantity(cart_id,quantity)
 
 @app.route('/cart/<cart_id>',methods=['DELETE'])
 def remove_item_from_cart(cart_id):
-    return delete_item(cart_id)
+    return CustomerCart.delete_item(cart_id)
 
 app.run()
