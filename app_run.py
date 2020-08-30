7cfea6ab29143ef203c651c70df2762e1e7c37d0
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-08-30 22:37:29 +0530

    app run is updated

--- app_run.py
+++ app_run.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask,g,session
 from categories.category_controller import *
 from items.item_controller import *
 from carts.cart_controller import *
 from user_status.login_controller import *
+from user_status.logout_controller import *
 
 app=Flask(__name__)
 app.config['DEBUG']=True
+app.secret_key='helloworld'
+
+
 
 app.register_blueprint(category_blueprint)
 app.register_blueprint(item_blueprint)
 app.register_blueprint(cart_blueprint)
 app.register_blueprint(login_blueprint)
+app.register_blueprint(logout_blueprint)
+app.register_blueprint(home_blueprint)
 
 if __name__=="__main__":
     app.run()
