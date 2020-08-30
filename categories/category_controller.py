f46ed74c4ce796e8e02462a51547a9b35f60bd52
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-08-30 22:30:53 +0530

    category_controller is updated

--- categories/category_controller.py
+++ categories/category_controller.py
@@ -1,8 +1,14 @@
-from flask import jsonify,Blueprint
+from flask import jsonify,Blueprint,render_template,session,flash
 from categories import category_dao
 
 category_blueprint=Blueprint('category_blueprint',__name__)
 
 @category_blueprint.route('/categories',methods=['GET'])
 def get_all_categories():
-    return jsonify(category_dao.get_categories())
+    if 'user_id' in session:
+        get_categories=(category_dao.get_categories())
+        return render_template('category.html',categories=get_categories)
+        #return jsonify(get_categories)
+    else:
+        flash("please login first","danger")
+        return render_template('login.html')
