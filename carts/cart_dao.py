4d4592671c253a283a1aaca766d025c26b6f1647
Balakumar5599 <52555273+Balakumar5599@users.noreply.github.com>
2020-08-31 07:28:49 +0530

    add to cart functionality is updated

--- carts/cart_dao.py
+++ carts/cart_dao.py
@@ -24,15 +24,25 @@ def get_cart_details(user_id):
 
 
 def insert_to_cart(user_id,item_id,quantity):
+
     max_quantity = db_obj.query(Item).filter_by(item_id=item_id).one()
+    cart_item=db_obj.query(Cart).filter_by(item_id=item_id).first()
+    item_price = db_obj.query(Item).filter_by(item_id=item_id).one()
+
     if int(quantity) > max_quantity.available_quantity:
         return "please enter the quantity less than available quantity"
-    else:
-        item_price = db_obj.query(Item).filter_by(item_id=item_id).one()
+
+    elif cart_item is None:
         item = Cart(user_id=user_id, item_id=int(item_id), quantity=int(quantity),total_price=int(quantity)*(item_price.item_price), buy_status=False)
         db_obj.add(item)
         db_obj.commit()
         return "Successfully item is post into cart"
+    else:
+        cart_item.quantity+=quantity
+        cart_item.total_price=(item_price.item_price)*cart_item.quantity
+        db_obj.add(cart_item)
+        db_obj.commit()
+        return "Sucessfully item is post into cart"
 
 
 def update_quantity(cart_id,quantity):
